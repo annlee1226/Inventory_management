95c201fc1bf704497f3e72cde7cb2c44fac5980e
Anna <alee531@ucr.edu>
2024-08-04 20:15:47 -0700

    Changes

--- firebase.js
+++ firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyARB9Hq85_N69Mmuz_OceRwNs-ILKlrcXk",
-  authDomain: "pantry-92f23.firebaseapp.com",
-  projectId: "pantry-92f23",
-  storageBucket: "pantry-92f23.appspot.com",
-  messagingSenderId: "53020062269",
-  appId: "1:53020062269:web:7ca97b3d3783d6a51db054",
-  measurementId: "G-9C7YKQRNBJ"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
